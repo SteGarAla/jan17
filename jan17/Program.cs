d4e5882d518f57610ab2422f874b9bb57fa7388c
stegarc <stegarc@EPSG1-825033>
2023-01-17 13:39:40 -0600

    Add project files.

---
+++ jan17/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jan17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
