18eae6f609102bf30601323d87ae3e646e466806
SteGarAla <stegarc@ut.utm.edu>
2023-01-17 14:25:01 -0600

    first push

--- jan17/Program.cs
+++ jan17/Program.cs
@@ -4,9 +4,37 @@ namespace jan17
 {
     class Program
     {
+        static void GuessingGame()
+        {
+            Random rando = new Random();
+            int randomNumber = rando.Next(0, 100);
+            Console.WriteLine("Welcome to the guessing Game");
+       
+            string userInput;
+            int userNumber = 0;
+           
+ 
+           while(userNumber != randomNumber)
+            {
+                //ask user for number
+                Console.WriteLine("Enter a number 1-100: ");
+                userInput = Console.ReadLine();
+                userNumber = Int32.Parse(userInput);
+
+                if(userNumber > randomNumber)
+                {
+                    Console.WriteLine($"{userNumber} is bigger than random number \n");
+                }
+                else if(userNumber < randomNumber)
+                {
+                    Console.WriteLine($"{userNumber} is smaller than random number \n");
+                }
+            }
+            Console.WriteLine("You guessed the number !!!");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GuessingGame();
         }
     }
 }
